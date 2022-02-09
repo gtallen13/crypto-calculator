ab37f483f397488fcffbddc9afed88b125a127c1
gtallen13 <gtallenpadi13@gmail.com>
2022-02-09 13:52:45 -0600

    responsive and real time calculation

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBitcoin,faEthereum,faMonero } from '@fortawesome/free-brands-svg-icons'
 import Select from 'react-select'
 import { useEffect, useState } from 'react'
+import  {toast,ToastContainer} from 'react-nextjs-toast'
 export default function Home({coins}) {
   const [currentCoin,setCurrentCoin] = useState('');
   const [buyPrice, setBuyPrice] = useState(0);
@@ -21,15 +21,33 @@ export default function Home({coins}) {
   })  
 
   useEffect(()=>{
-    setBuyPrice(currentCoin.value)
-    setSellPrice(currentCoin.value+(currentCoin.value*0.50))
+    if (currentCoin.value)
+    {
+      const newBuyPrice = currentCoin.value.toFixed(6)
+      const newSellPrice = currentCoin.value + currentCoin.value*0.50
+      setBuyPrice(newBuyPrice)
+      setSellPrice(newSellPrice)  
+    }
   },[currentCoin.value])
-
+  useEffect(()=>{
+    if(currentCoin){
+      calculateTotal()
+    }
+  })
   const calculateTotal = ()=>{
-    const purchasedStock = investment/buyPrice;
-    const soldStock = purchasedStock*sellPrice;
-    setNetTotal(soldStock - investment);
+    if (currentCoin.value){
+      const purchasedStock = investment/buyPrice;
+      const soldStock = purchasedStock*sellPrice;
+      const newNetTotal = soldStock - investment 
+      setNetTotal(newNetTotal.toFixed(6));
+    }
+    else{
+      toast.notify("Please select a coin",{
+        title:"Something's missing..."      
+      })
+    }
   }
+  
   return (
     <div className='container center'>
       <div className='title'>
@@ -39,6 +57,7 @@ export default function Home({coins}) {
           <FontAwesomeIcon icon={faMonero}/>
         </div>
         <h1>Crypto Profit Calculator</h1>
+        <ToastContainer align={"right"} />
       </div>
       <form className={styles.frmCrypto}>
         <div className={styles.frmRow}>
@@ -57,21 +76,22 @@ export default function Home({coins}) {
           <label className={styles.frmLabel}>Coin</label>
           <Select
           options={options}
-          className={styles.frmInput}
+          className={`${styles.frmInput} ${styles.selectInput}`}
           defaultValue={currentCoin}
           onChange={setCurrentCoin}
           />
         </div>
-        {currentCoin ?(<span>{currentCoin.symbol} {`$${currentCoin.value}`}</span>):null}
-        {netTotal ? (<span>Total: {netTotal}</span>):null}
-        <button type='button' onClick={()=>calculateTotal()}>Calculate</button>
+        {currentCoin ?(<span>{currentCoin.symbol} price: {`$${currentCoin.value}`}</span>):null}
+        {netTotal ? (
+        <span className={netTotal > 0 ? 'positive':'negative'}>Net Total: {netTotal}</span>
+        ):null}
       </form>
     </div>
-  )
+  ) 
 }
 
 export async function getServerSideProps(context){
-  const endpoint = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
+  const endpoint = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_p age=50&page=1&sparkline=false'
   const data = await fetch(endpoint)
   const coins = await data.json()
 
